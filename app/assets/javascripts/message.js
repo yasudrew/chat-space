e78a3723e2e885b301b7a043fab367d7160cd246
yasudrew <63987719+yasudrew@users.noreply.github.com>
2020-04-28 20:31:09 +0900

    automation-banch

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="main-chat__message-list__message">
+       `<div class="main-chat__message-list__message" data-message-id=${message.id}>
           <div class="main-chat__message-list__message__message-data">
             <p class="main-chat__message-list__message__message-data__username">
               ${message.user_name}
@@ -19,7 +19,7 @@ $(function(){
       return html;
     } else {
       var html =
-      `<div class="main-chat__message-list__message">
+      `<div class="main-chat__message-list__message" data-message-id=${message.id}>
         <div class="main-chat__message-list__message__message-data">
           <p class="main-chat__message-list__message__message-data__username">
             ${message.user_name}
@@ -58,5 +58,30 @@ $(function(){
       alert("メッセージ送信に失敗しました");
       $('.main-chat__message-form__input-box__submit-btn').prop('disabled', false);
     });
-  })
+  });
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__message-list').append(insertHTML);
+        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  }; 
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
